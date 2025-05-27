50b390d080f6dd062b4d24a5fed184039a9f6b1b
Alaa M. Jaddou <alaa@aj-group.ps>
2025-05-27 21:51:32 +0300

    - Add Ahref verification file.
    Signed-off-by: Alaa M. Jaddou <alaa@aj-group.ps>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <script src="https://analytics.ahrefs.com/analytics.js" data-key="z/TLyFKWDR1n/g0mI8lW+A" async></script>
+    <meta name="ahrefs-site-verification" content="9bd1d37348a6868876eaffc532f9f6b5042f8ea970de4f7f28b757de7f82a93f">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
