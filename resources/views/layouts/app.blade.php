98deb72c148effde885a2d4de6c4bb77f30bdf4e
Alaa M. Jaddou <alaa@aj-group.ps>
2025-06-15 22:39:49 +0300

    - add the canonical link. - update version.
    Signed-off-by: Alaa M. Jaddou <alaa@aj-group.ps>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
 
     <title>@yield('title', 'Alaa M. Jaddou - Senior Software Engineer')</title>
 
+    <link name="canonical" rel="canonical" href="{{ url()->current() }}">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
