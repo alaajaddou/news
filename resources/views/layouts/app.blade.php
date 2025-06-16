d7e142711eb3e8a3698638ce19bdbb4795960920
Alaa M. Jaddou <alaa@aj-group.ps>
2025-06-16 22:32:13 +0300

    - adding favicons
    Signed-off-by: Alaa M. Jaddou <alaa@aj-group.ps>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,18 @@
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:type" content="website">
 
+    <!-- Favicon and Touch Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('favicons/site.webmanifest') }}">
+    <link rel="icon" href="{{ asset('favicons/favicon.ico') }}">
+
+    <!-- Android Chrome Icons -->
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicons/android-chrome-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('favicons/android-chrome-512x512.png') }}">
+
+
     <title>@yield('title', 'Alaa M. Jaddou - Senior Software Engineer')</title>
 
     <link name="canonical" rel="canonical" href="{{ url()->current() }}">
