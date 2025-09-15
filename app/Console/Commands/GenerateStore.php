cc50907843efcce74b34837aea68bfc0b8d4e5f2
Alaa M. Jaddou <alaa@aj-group.ps>
2025-09-15 23:44:12 +0300

    - Add Stores.
    Signed-off-by: Alaa M. Jaddou <alaa@aj-group.ps>

--- app/Console/Commands/GenerateStore.php
+++ app/Console/Commands/GenerateStore.php
@@ -290,7 +290,7 @@ class GenerateStore extends Command
 		$this->logProgress("Start Installing Magento with the following parameters:");
 		$this->logProgress("Magento Store Id: {$storeId}");
 
-		$cmd = "sudo php -d memory_limit=2G {$mage_root}/bin/magento setup:install "
+		$cmd = "sudo php -d max_execution_time=7200 -d memory_limit=2G {$mage_root}/bin/magento setup:install "
 			. "--cleanup-database "
 			. "--base-url={$baseurl} "
 			. "--db-host={$dbHost} "
