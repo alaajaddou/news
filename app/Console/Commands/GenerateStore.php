2c4d43e1a9c72e2c020ccddf1bc0e2606978004a
Alaa M. Jaddou <alaa@aj-group.ps>
2025-09-15 18:51:53 +0300

    - Add Stores.
    Signed-off-by: Alaa M. Jaddou <alaa@aj-group.ps>

--- app/Console/Commands/GenerateStore.php
+++ app/Console/Commands/GenerateStore.php
@@ -39,7 +39,7 @@ class GenerateStore extends Command
 			$baseUrl = "https://{$storeId}.aj-group.ps/";
 
 			$mageRoot = "/var/www/htdocs/stores/{$storeId}";
-			$src = __DIR__ . '/store_example';
+			$src = '/var/www/htdocs/website/store_example';
 
 			// =================== STEP 1: COPY STORE ===================
 			$this->info("→ Copying store files...");
